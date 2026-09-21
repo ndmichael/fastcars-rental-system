5f5991883b2f683d5960e8cb00460980f471f34a
ndmichael <ukejemicheal@gmail.com>
2026-09-21 22:17:15 +0100

    Setup templates routing

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ("customer", "Customer"),
+        ("admin", "Admin"),
+    ]
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="customer",
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+    )
+
+    def save(self, *args, **kwargs):
+        self.username = self.username.strip().lower()
+        self.email = self.email.strip().lower()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
