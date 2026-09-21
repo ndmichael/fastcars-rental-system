52dfe9bb2f7895570a5e4ca0e0eaaf52c6762d7c
ndmichael <ukejemicheal@gmail.com>
2026-09-21 22:19:01 +0100

    templates routing in settings

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+AUTH_USER_MODEL = "accounts.User"
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
